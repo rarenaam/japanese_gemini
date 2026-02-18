f3b2eedb454e517381ce574584486bfc26cf131b
rarenaam <100432085+rarenaam@users.noreply.github.com>
2026-02-18 08:05:15 +0100

    Create woorden.js

---
+++ js/woorden.js
@@ -0,0 +1,5 @@
+const alleWoorden = [
+    {jp: "こんにちは", nl: "hallo", romaji: "konnichiwa", cat: "groeten"},
+    {jp: "ねこ", nl: "kat", romaji: "neko", cat: "dieren"},
+    // Voeg hier de rest van je lijst toe...
+];
